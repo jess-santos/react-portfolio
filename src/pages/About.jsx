023d12e9f003028be48eae13cdea68a64eb8eba8
[jess santos] <jesssantos@Jesss-MBP.cust.communityfibre.co.uk>
2024-03-02 16:13:44 +0000

    Basic layout set up

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Container from '../components/Container';
+import Row from '../components/Row';
+import Col from '../components/Col';
+
+function About() {
+  return (
+    <div>
+      <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
+        <h1>Pupster</h1>
+        <h2>They're the Good Boys and Girls</h2>
+      </Hero>
+    </div>
+  );
+}
+
+export default About;
